57b1db552a1b044f8712a492bb9e91b3667b1773
anesveth <annesveth@ufm.edu>
2019-04-09 08:38:08 -0600

    Fase3>Menu y se cambio el lenguaje de espa;ol a ingles

--- totitoo.py
+++ totitoo.py
@@ -1,27 +1,78 @@
 import random
 #Variables usadas en funciones distintas aparecen afuera/antes de ellas
-def screen():
+
+def screen1v1():
     '''
-    Aparece inicial mostrando las coordenadas del totito
+    Pantalla inicial modo Player vs player
+    Aparece inicialmente mostrando las coordenadas del totito
     '''
-    print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵
+    print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵                                
+                ///// 1  v  1  /////
 
-    QWE      _|_|_
-    ASD   ➤  _|_|_
-    ZXC       | |
-
-
+    QWE       _|_|_
+     ASD   ➤  _|_|_
+      ZXC      | |
+        the first player to get three in a row of their mark wins
         let's start!
     """)
-print(screen())
+def screen1vC():
+    '''
+    Pantalla inicial modo Player vs computer
+    Aparece inicialmente mostrando las coordenadas del totito
+    '''
+    print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵                                
+                ///// player  v  computer  /////
+
+    QWE       _|_|_
+     ASD   ➤  _|_|_
+      ZXC      | |
+        try to get three in a row
+        let's start!
+    """)
+def screenmisere():
+    '''
+    Pantalla inicial modo Player vs Player MISERE
+    Aparece inicialmente mostrando las coordenadas del totito
+    '''
+    print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵                                
+                ///// 1  v  1  MISERE  /////
+
+    QWE       _|_|_
+     ASD   ➤  _|_|_
+      ZXC      | |
+        get your opponent to make three in a row
+        let's start!
+    """)
+def menu():
+    '''Menu principal de opciones'''
+    print("""︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵
+                
+                T O T I T O    U・ᴥ・U  
+
+
+                1) Player vs Player
+
+                2) Player vs Computer
+
+                3) Misere
+
+                    ₍ᐢ•ﻌ•ᐢ₎*･ﾟ｡
+    """)
+print (menu())
+selection=input("""Choose your game mode ➤ """)
+print("︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵")
 players=[]
-player1=input("Jugador 1: ")
-players.append(player1)
-player2=input("Jugador 2: ")
-players.append(player2)
-first=random.choice(players)
-players.remove(first)
-second=players[0]
+if (selection=="1") or (selection=="3"):
+    player1=input("Player 1: ")
+    players.append(player1)
+    player2=input("Player 2: ")
+    players.append(player2)
+    first=random.choice(players)
+    players.remove(first)
+    second=players[0]
+if selection=="2":
+    player=input("Your name: ")
+    players.append(player)
 Iconos=["X","O"]
 coordinates=[
 "Q","W","E",
@@ -40,9 +91,9 @@ def jugada():
     '''
     Pide coordenadas para la jugada del primer jugador
     '''
-    play1=input("seleccione la coordenada de su jugada: ")
+    play1=input("Choose the coordinate of your move: ")
     if play1 in usedcoordinates:
-        print("Ya esta jugada esa opcion")
+        print("This option has already been played")
         #EVITA QUE EL JUGADOR CORRIJA/REEMPLAZE JUGADAS SUYAS O DE SU OPONENTE
     elif play1 in coordinates: 
         parts[coordinates.index(play1)]=icono_en_uso
@@ -51,9 +102,9 @@ def jugadacontrincante():
     '''
     Pide coordenadas para la jugada del segundo jugador
     '''
-    play1=input("seleccione la coordenada de su jugada: ")
+    play1=input("Choose the coordinate of your move: ")
     if play1 in usedcoordinates:
-        print("Ya esta jugada esa opcion")
+        print("This option has already been played")
         #EVITA QUE EL JUGADOR CORRIJA/REEMPLAZE JUGADAS SUYAS O DE SU OPONENTE
     elif play1 in coordinates: 
         parts[coordinates.index(play1)]=icono_oponente
@@ -62,32 +113,26 @@ def turn1():
     '''
     Muestra al primer jugador (elejido al azar), su icono(X o O) y le avisa que es su turno
     '''
-    print ("Turno de el jugador {}, Icono: ".format(first)+"{}".format(icono_en_uso))
+    print ("Turn of player {}, Icon: ".format(first)+"{}".format(icono_en_uso))
     print(jugada())
 def turn2():
     '''
     Muestra al segundo jugador que quedo, su icono(X o O) y le avisa que es su turno
     '''
-    print ("Turno de el jugador {}, Icono: ".format(second)+"{}".format(icono_oponente))
+    print ("Turn of player {}, Icon: ".format(second)+"{}".format(icono_oponente))
     print(jugadacontrincante())
 def table():
     '''Tabla del totito en la cual se van mostrando las jugadas'''
     tablero=[\
-    ["         |         |         "],\
-    ["         |         |         "],\
-    ["   {}     |    {}    |    {}    ".format(parts[0],parts[1],parts[2])],\
-    ["         |         |         "],\
-    ["_________|_________|_________"],\
-    ["         |         |         "],\
-    ["         |         |         "],\
-    ["   {}     |    {}    |    {}    ".format(parts[3],parts[4],parts[5])],\
-    ["         |         |         "],\
-    ["_________|_________|_________"],\
-    ["         |         |         "],\
-    ["         |         |         "],\
-    ["   {}     |    {}    |    {}   ".format(parts[6],parts[7],parts[8])],\
-    ["         |         |         "],\
-    ["         |         |         "]]
+    ["        |        |        "],\
+    ["   {}    |    {}   |    {}   ".format(parts[0],parts[1],parts[2])],\
+    ["________|________|________"],\
+    ["        |        |        "],\
+    ["   {}    |    {}   |    {}   ".format(parts[3],parts[4],parts[5])],\
+    ["________|________|________"],\
+    ["        |        |        "],\
+    ["   {}    |    {}   |    {}  ".format(parts[6],parts[7],parts[8])],\
+    ["        |        |        "]]
     
     for i in range(len(tablero)):
         print (*tablero[i])
@@ -117,16 +162,16 @@ def gamemode1v1():
         for i in range(8):#COMPROBACION DE QUE HAYA GANADO
             if (wincombos[i]==winningXcase[0])and(ganador=="n"): 
                 if icono_en_uso=="X":
-                    print("Jugador {} gana!".format(first))
+                    print("Player {} wins!".format(first))
                     ganador=first
                 else:
-                    print("Jugador {} gana!".format(second))
+                    print("Player {} wins!".format(second))
                     ganador=second   
             if (wincombos[i]==winningOcase[0])and(ganador=="n"): 
                 if icono_en_uso=="O":
-                    print("Jugador {} gana!".format(first))
+                    print("Player {} wins!".format(first))
                     ganador=first
                 else:
-                    print("Jugador {} gana!".format(second))
+                    print("Player {} wins!".format(second))
                     ganador=second   
 print(gamemode1v1())
