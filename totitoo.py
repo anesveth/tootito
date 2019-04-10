01e23b9f5a507bb3377eb4c38edbf6204a58d15a
anesveth <annesveth@ufm.edu>
2019-04-09 18:26:36 -0600

    Fase2 completa

--- totitoo.py
+++ totitoo.py
@@ -7,7 +7,7 @@ def screen1v1():
     Aparece inicialmente mostrando las coordenadas del totito
     '''
     print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵                                
-                ///// 1  v  1  /////
+                ///// 1  vs  1  /////
 
     QWE       _|_|_
      ASD   ➤  _|_|_
@@ -15,13 +15,13 @@ def screen1v1():
         the first player to get three in a row of their mark wins
         let's start!
     """)
-def screen1vC():
+def screen1vc():
     '''
     Pantalla inicial modo Player vs computer
     Aparece inicialmente mostrando las coordenadas del totito
     '''
     print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵                                
-                ///// player  v  computer  /////
+            ///// player  vs  computer  /////
 
     QWE       _|_|_
      ASD   ➤  _|_|_
@@ -35,7 +35,7 @@ def screenmisere():
     Aparece inicialmente mostrando las coordenadas del totito
     '''
     print("""︵‿︵‿︵‿︵‿︵  T O T I T O    U・ᴥ・U  ‿︵‿︵‿︵‿︵‿︵‿︵                                
-                ///// 1  v  1  MISERE  /////
+                ///// 1  vs  1  MISERE  /////
 
     QWE       _|_|_
      ASD   ➤  _|_|_
@@ -62,7 +62,8 @@ print (menu())
 selection=input("""Choose your game mode ➤ """)
 print("︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵")
 players=[]
-if (selection=="1") or (selection=="3"):
+if (selection=="1"):
+    print(screen1v1())
     player1=input("Player 1: ")
     players.append(player1)
     player2=input("Player 2: ")
@@ -70,9 +71,39 @@ if (selection=="1") or (selection=="3"):
     first=random.choice(players)
     players.remove(first)
     second=players[0]
+
 if selection=="2":
+    print(screen1vc())
     player=input("Your name: ")
     players.append(player)
+    computernames=[
+    "Santa","Kenny vel","Champion","Ella no te ama","Sus mentiras","Justin Bieber","GamerGirl",
+    "Chum Chum","Britney Spears","Katy Perry","One Direction","Mickey Mouse","Kim Kardashian",
+    "Mcdonald's","La niña del Aro","Aros de cebolla","Minion","Harry Potter",
+    "Voldemort","Larry is real","Badabun","Dora the explorer","Gucci","Gucci Gang","Fangirl",
+    "Harry Styles","Louis Tomlinson","Shawn Mendes"
+    ]
+    titlesforthename=["Son of","King of","Slave of","Fan of","Duchess","Killer of",
+    "President","Lover of","Queen","Pray for","Death to","Hates","Boyfriend of","Girlfriend of"]
+    #hace un nombre a base de las dos listas
+    computername=str(random.choice(titlesforthename))+" "+str(random.choice(computernames))
+    print("YOUR OPPONENT IS ( •̀ω•́ )σ {}".format(computername))
+    players.append(computername)
+    first=random.choice(players)
+    players.remove(first)
+    second=players[0]
+
+
+if (selection=="3"):
+    print(screenmisere())
+    player1=input("Player 1: ")
+    players.append(player1)
+    player2=input("Player 2: ")
+    players.append(player2)
+    first=random.choice(players)
+    players.remove(first)
+    second=players[0]
+
 Iconos=["X","O"]
 coordinates=[
 "Q","W","E",
@@ -84,9 +115,9 @@ parts=[" "," "," ",
 " "," "," ",
 " "," "," "
 ]
-icono_en_uso=random.choice(Iconos)
+icono_en_uso=random.choice(Iconos)#Icono del primer jugador
 Iconos.remove(icono_en_uso)
-icono_oponente=Iconos[0]
+icono_oponente=Iconos[0]#Icono del segundo jugador
 def jugada():
     '''
     Pide coordenadas para la jugada del primer jugador
@@ -95,9 +126,11 @@ def jugada():
     if play1 in usedcoordinates:
         print("This option has already been played")
         #EVITA QUE EL JUGADOR CORRIJA/REEMPLAZE JUGADAS SUYAS O DE SU OPONENTE
+        print(jugada())
     elif play1 in coordinates: 
         parts[coordinates.index(play1)]=icono_en_uso
         usedcoordinates.append(play1)
+
 def jugadacontrincante():
     '''
     Pide coordenadas para la jugada del segundo jugador
@@ -105,10 +138,12 @@ def jugadacontrincante():
     play1=input("Choose the coordinate of your move: ")
     if play1 in usedcoordinates:
         print("This option has already been played")
+        print(jugada())
         #EVITA QUE EL JUGADOR CORRIJA/REEMPLAZE JUGADAS SUYAS O DE SU OPONENTE
     elif play1 in coordinates: 
         parts[coordinates.index(play1)]=icono_oponente
         usedcoordinates.append(play1)
+    
 def turn1():
     '''
     Muestra al primer jugador (elejido al azar), su icono(X o O) y le avisa que es su turno
@@ -121,39 +156,102 @@ def turn2():
     '''
     print ("Turn of player {}, Icon: ".format(second)+"{}".format(icono_oponente))
     print(jugadacontrincante())
+
+def jugadacomputadora():
+    '''Algoritmo modo de juego de la computadora'''
+    if first==computername:#si la computadora juega primero
+        play1=random.choice(coordinates)
+        while (play1 in usedcoordinates):
+            play1=random.choice(coordinates)
+        usedcoordinates.append(play1)
+        parts[coordinates.index(play1)]=icono_oponente
+    elif second==computername:#si la computadora juega de segundas
+        play1=random.choice(coordinates)
+        while (play1 in usedcoordinates):
+            play1=random.choice(coordinates)
+        usedcoordinates.append(play1)
+        parts[coordinates.index(play1)]=icono_oponente
+
+def turn1_1vsc():
+    '''
+    primer turno, puede ser player o computadora
+    '''
+    print ("Turn of player {}, Icon: ".format(first)+"{}".format(icono_en_uso))
+    if first==player:
+        play1=input("Choose the coordinate of your move: ")
+        if play1 in usedcoordinates:
+            print("This option has already been played")
+            #EVITA QUE EL JUGADOR CORRIJA/REEMPLAZE JUGADAS SUYAS O DE SU OPONENTE
+            print(jugada())
+        elif play1 in coordinates: 
+            parts[coordinates.index(play1)]=icono_en_uso
+            usedcoordinates.append(play1)
+    elif first==computername:#si empieza la computadora la partida, su seleccion sera al azar
+        print(jugadacomputadora())
+
+def turn2_1vsc():    
+    '''
+    segundo turno, puede ser player o computadora
+    '''
+    print ("Turn of player {}, Icon: ".format(second)+"{}".format(icono_oponente))
+    if second==player:
+        play1=input("Choose the coordinate of your move: ")
+        if play1 in usedcoordinates:
+            print("This option has already been played")
+            #EVITA QUE EL JUGADOR CORRIJA/REEMPLAZE JUGADAS SUYAS O DE SU OPONENTE
+            print(jugada())
+        elif play1 in coordinates: 
+            parts[coordinates.index(play1)]=icono_oponente
+            usedcoordinates.append(play1)
+    elif second==computername:#si empieza la computadora la partida, su seleccion sera al azar
+        print(jugadacomputadora())
+
 def table():
     '''Tabla del totito en la cual se van mostrando las jugadas'''
     tablero=[\
-    ["        |        |        "],\
-    ["   {}    |    {}   |    {}   ".format(parts[0],parts[1],parts[2])],\
-    ["________|________|________"],\
-    ["        |        |        "],\
-    ["   {}    |    {}   |    {}   ".format(parts[3],parts[4],parts[5])],\
-    ["________|________|________"],\
-    ["        |        |        "],\
-    ["   {}    |    {}   |    {}  ".format(parts[6],parts[7],parts[8])],\
-    ["        |        |        "]]
+    ["        |       |        "],\
+    ["   {}    |   {}   |    {}   ".format(parts[0],parts[1],parts[2])],\
+    ["________|_______|________"],\
+    ["        |       |        "],\
+    ["   {}    |   {}   |    {}   ".format(parts[3],parts[4],parts[5])],\
+    ["________|_______|________"],\
+    ["        |       |        "],\
+    ["   {}    |   {}   |    {}  ".format(parts[6],parts[7],parts[8])],\
+    ["        |       |        "]]
     
     for i in range(len(tablero)):
         print (*tablero[i])
 winningXcase=[("X","X","X")]
 winningOcase=[("O","O","O")]
 
-def turnos():
+def turnos1v1():
     '''
     Junta las funciones anteriores
-    generando el sistema de juego
+    generando el sistema de juego 1 vs 1
     '''
     print(turn1())
     print(table())
     print(turn2())
     print(table())
 
+def turnos1vc():
+    '''
+    Junta las funciones anteriores
+    generando el sistema de juego 1 vs computer
+    '''
+    print(turn1_1vsc())
+    print(table())
+    print(turn2_1vsc())
+    print(table())
+
 ganador="n"
 def gamemode1v1():
+    '''
+    Modo de juego 1v1 
+    '''
     ganador="n"
     while(ganador=="n"):##El juego se repite hasta que alguien gana.
-        print(turnos())
+        print(turnos1v1())
         wincombos=[(parts[0],parts[1],parts[2]),(parts[3],parts[4],parts[5]),
         (parts[6],parts[7],parts[8]),(parts[0],parts[3],parts[6]),
         (parts[6],parts[4],parts[2]),(parts[0],parts[4],parts[8]),
@@ -173,5 +271,40 @@ def gamemode1v1():
                     ganador=first
                 else:
                     print("Player {} wins!".format(second))
+                    ganador=second 
+def gamemode1vc():
+    '''
+    Modo de juego 1vComputer 
+    '''  
+    ganador="n"
+    while(ganador=="n"):##El juego se repite hasta que alguien gana.
+        print(turnos1vc())
+        wincombos=[(parts[0],parts[1],parts[2]),(parts[3],parts[4],parts[5]),
+        (parts[6],parts[7],parts[8]),(parts[0],parts[3],parts[6]),
+        (parts[6],parts[4],parts[2]),(parts[0],parts[4],parts[8]),
+        (parts[2],parts[5],parts[8]),(parts[1],parts[4],parts[7])
+        ]
+        for i in range(8):#COMPROBACION DE QUE HAYA GANADO
+            if (wincombos[i]==winningXcase[0])and(ganador=="n"): 
+                if icono_en_uso=="X":
+                    print("Player {} wins!".format(first))
+                    ganador=first
+                else:
+                    print("Player {} wins!".format(second))
                     ganador=second   
-print(gamemode1v1())
+            if (wincombos[i]==winningOcase[0])and(ganador=="n"): 
+                if icono_en_uso=="O":
+                    print("Player {} wins!".format(first))
+                    ganador=first
+                else:
+                    print("Player {} wins!".format(second))
+                    ganador=second 
+############################################################################################################
+###########################SE ACTIVA EL PROGRAMA DEPENDE DE LO ELEJIDO EN EL MENU ##########################
+
+if (selection=="1"):
+    print(gamemode1v1())
+if (selection=="2"):
+    print(gamemode1vc())
+
+
